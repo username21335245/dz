5a828ff44213d9f8a0f6881043805a9187874e76
username21335245 <95102197+username21335245@users.noreply.github.com>
2022-05-11 22:24:10 +0300

    Add files via upload

---
+++ task_6/task_6/task_6.cpp
@@ -0,0 +1,53 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+bool solution(string str1, string str2) {
+    bool res = false;
+    int n = (int)str2.size();
+    bool* infostr2 = new bool[n];
+    for (int i = 0; i < n; i++) {
+        infostr2[i] = false;
+    }
+
+    for (int i = 0; i < str1.size(); i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (infostr2[j]) {
+                continue;
+            }
+            if (str1[i] == str2[j]) {
+                infostr2[j] = true;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < str2.size(); i++) {
+        if (infostr2[i]) {
+            res = true;
+        }
+        else {
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
+
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+    string str1, str2;
+    printf("Введите первое слово: ");
+    cin >> str1;
+    printf("Введите второе слово: ");
+    cin >> str2;
+    if (solution(str1, str2)) {
+        printf("Да, можно");
+    }
+    else {
+        printf("Нет, нельзя");
+    }
+}
--- task_7/task_6/task_6.cpp
+++ task_7/task_6/task_6.cpp
@@ -1,72 +1,140 @@
-﻿#include "stdio.h"
-#define _USE_MATH_DEFINES
-#include "math.h"
-#include "string"
-#include "conio.h"
-#include "iostream"
-using std::cout;
-using std::cin;
-using std::endl;
+﻿
 
-void inputArray(char* str1, char* str2) {                    // объявление строк и переменных
-    cout << "введите строку1: ";
-    cin >> str1;
-    cout << "введите строку2: ";
-    cin >> str2;
-}
+#include <iostream>
 
-int how_many_simvol_in_str(char simb, char* str)
-{
-    int result = 0;
-    for (int i = 0; str[i]; i++)
-        if (str[i] == simb)
-            result++;
-    return result;
-}
-
-
-bool checkArray(char* str1, char* str2) {    // первая проверка
-    if (strlen(str1) != strlen(str2))
-        return false;
-    for (int i = 0; str1[i]; i++)
-        if (how_many_simvol_in_str(str1[i], str1) != how_many_simvol_in_str(str1[i], str2))
-            return false;
-    return true;
-}
-
-
-bool checkArray2(char* str1, char* str2) {       // вторая проверка
-    if (strlen(str1) != strlen(str2))
-        return false;
-
-    int chars1[256], chars2[256];
-    memset(chars1, 0, sizeof(chars1));
-    memset(chars2, 0, sizeof(chars2));
-
-    for (int i = 0; str1[i]; i++) {
-        chars1[str1[i]]++;
-        chars2[str2[i]]++;
+int** createAndFillArray(int h, int w) {
+    int** array = new int* [h];
+    for (int i = 0; i < h; i++)
+    {
+        array[i] = new int[w];
+        for (int j = 0; j < w; j++) {
+            printf("Введите array[%d][%d] : ", i, j);
+            scanf_s("%d", &array[i][j]);
+        }
     }
-
-    for (int i = 0; i < 256; i++)
-        if (chars1[i] != chars2[i])
-            return false;
-    return true;
+    return array;
+}
+void printArray(int** arr, int rows, int columns) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j)
+            std::cout << arr[i][j] << ' ';
+        std::cout << std::endl;
+    }
+}
+int** staticArray() {
+    int arrayS[5][7] = {
+        {1, 0, 2, 2, 3, 3, 7},
+        {1, 5, 5, 0, 3, 3, 7},
+        {0, 5, 0, 4, 8, 0, 7},
+        {9, 9, 6, 0, 7, 8, 0},
+        {9, 2, 4, 8, 8, 0, 0},
+    };
+    int** array = new int* [5];
+    for (int i = 0; i < 5; i++)
+    {
+        array[i] = new int[7];
+        for (int j = 0; j < 7; j++) {
+            array[i][j] = arrayS[i][j];
+        }
+    }
+    return array;
 }
 
+bool sosediRavni(int** arr, int i, int j, bool n1, bool n2, bool n3, bool n4, bool n5, bool n6, bool n7, bool n8) {
+    int count = 0;
+    if (n1) { count += (arr[i][j] == arr[i - 1][j - 1]) ? 1 : 0; }
+    if (n2) { count += (arr[i][j] == arr[i - 1][j]) ? 1 : 0; }
+    if (n3) { count += (arr[i][j] == arr[i - 1][j + 1]) ? 1 : 0; }
+    if (n4) { count += (arr[i][j] == arr[i][j + 1]) ? 1 : 0; }
+    if (n5) { count += (arr[i][j] == arr[i + 1][j + 1]) ? 1 : 0; }
+    if (n6) { count += (arr[i][j] == arr[i + 1][j]) ? 1 : 0; }
+    if (n7) { count += (arr[i][j] == arr[i + 1][j - 1]) ? 1 : 0; }
+    if (n8) { count += (arr[i][j] == arr[i][j - 1]) ? 1 : 0; }
+    return count >= 2;
+}
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    char str1[1000], str2[1000];
-    int l, x;
-    inputArray((char*)&str1, (char*)&str2);
+bool supportingSolion(int** arr, int rows, int columns, int i, int j) {
+    if (i == 0 && j == 0) {
+        if (sosediRavni(arr, i, j, false, false, false, true, true, true, false, false)) return true;
 
-    if (checkArray((char*)&str1, (char*)&str2) && checkArray2((char*)&str1, (char*)&str2)) {
-        cout << "Можно" << endl;
+    }
+    else if (i == 0 && j == columns - 1) {
+        if (sosediRavni(arr, i, j, false, false, false, false, false, true, true, true)) return true;
+
+    }
+    else if (i == rows - 1 && j == 0) {
+        if (sosediRavni(arr, i, j, false, true, true, true, false, false, false, false)) return true;
+
+    }
+    else if (i == rows - 1 && j == columns - 1) {
+        if (sosediRavni(arr, i, j, true, true, false, false, false, false, false, true)) return true;
+
+    }
+    else if (i == 0) {
+        if (sosediRavni(arr, i, j, false, false, false, true, true, true, true, true)) return true;
+
+    }
+    else if (j == 0) {
+        if (sosediRavni(arr, i, j, false, true, true, true, true, true, false, false)) return true;
+
+    }
+    else if (i == rows - 1) {
+        if (sosediRavni(arr, i, j, true, true, true, true, false, false, false, true)) return true;
+
+    }
+    else if (j == columns - 1) {
+        if (sosediRavni(arr, i, j, true, true, false, false, false, true, true, true)) return true;
+
     }
     else {
-        cout << "Нельзя" << endl;
+        if (sosediRavni(arr, i, j, true, true, true, true, true, true, true, true)) return true;
     }
+    return false;
+}
 
-    return 0;
+int** solition(int** arr, int rows, int columns) {
+    int** array = new int* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        array[i] = new int[columns];
+        for (int j = 0; j < columns; j++) {
+            if (supportingSolion(arr, rows, columns, i, j)) { array[i][j] = 1; }
+            else { array[i][j] = 0; }
+        }
+    }
+    return array;
+}
+
+void run(int** arr, int rows, int columns) {
+    printf("\nВходной массив:\n\n");
+    printArray(arr, rows, columns);
+    printf("\n");
+    arr = solition(arr, rows, columns);
+    printf("Выходной массив:\n\n");
+    printArray(arr, rows, columns);
+    delete[] arr;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    printf("Хотите ввести вручную или использовать пример? ( 1 / 2 )\n");
+    int choose;
+    scanf_s("%d", &choose);
+    int rows, columns;
+    if (choose == 1) {
+        printf("Введите высоту массива : ");
+        scanf_s("%d", &rows);
+        printf("Введите ширину массива : ");
+        scanf_s("%d", &columns);
+        run(createAndFillArray(rows, columns), rows, columns);
+    }
+    else if (choose == 2) {
+        rows = 5;
+        columns = 7;
+        run(staticArray(), rows, columns);
+    }
+    else {
+        printf("Ввод неверный");
+    }
 }
